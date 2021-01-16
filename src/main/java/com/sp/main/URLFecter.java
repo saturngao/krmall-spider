29c62f72d3f0e5941b5d79c041a733a254930332
Gao.Chao.Chao <gao.chao.chao@vertiv.com>
2021-01-16 18:13:58 +0800

    学习

--- src/main/java/com/sp/main/URLFecter.java
+++ src/main/java/com/sp/main/URLFecter.java
@@ -178,7 +178,7 @@ public class URLFecter {
 //                        }
 //
 //                    }
-                }
+//                }
 
 
 //                System.out.println(doc.select("thumbnail"));
