4762bf4a3c7c38ac9d61058442f1788376c7e89b
Gao.Chao.Chao <gao.chao.chao@vertiv.com>
2021-01-20 17:12:51 +0800

    学习

--- src/main/java/com/sp/main/MainClass.java
+++ src/main/java/com/sp/main/MainClass.java
@@ -1,5 +1,6 @@
 package com.sp.main;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.sp.main.excel.ExcelUtils;
@@ -29,7 +30,7 @@ public class MainClass {
         List<String> listTit = Arrays.asList(titles);
         CloseableHttpClient httpclient = HttpClients.createDefault();
         //我们要爬取的一个地址，这里可以从数据库中抽取数据，然后利用循环，可以爬取一个URL队列
-        String url = "https://store.coupang.com/vp/vendors/A00163079/product/lists?outboundShippingPlaceId=&attributeFilters=&brand=&componentId=&keyword=&maxPrice=&minPrice=&pageNum=1&rating=0&sortTypeValue=&scpLanding=true";
+        String url = "https://store.coupang.com/vp/vendors/A00250830/product/lists?outboundShippingPlaceId=&attributeFilters=&brand=&componentId=&keyword=&maxPrice=&minPrice=&pageNum=1&rating=0&sortTypeValue=&scpLanding=true";
 
         //将抓取的数据插入数据库
         Map<String, String> header = new HashMap<>();
@@ -59,8 +60,16 @@ public class MainClass {
             values.add(value);
             JSONObject images = URLFecter.getMethod(String.format(imageUrl, product.getString("productId"), product.getString("itemId"), product.getString("vendorItemId")), header);
             System.out.println(images);
-            List<String> iList = URLFecter.urlParseImage(images.getJSONArray("details").getJSONObject(0).getJSONArray("vendorItemContentDescriptions").getJSONObject(0).getString("content"));
-            imgPath.append("H:\\Downloads\\img\\").append(product.getString("productId")).append("\\")
+            List<String> iList = new ArrayList<>();
+            for (Object o : images.getJSONArray("details")) {
+                if ("HTML".equals(JSON.parseObject(o.toString()).getString("contentType"))){
+                    iList.addAll(URLFecter.urlParseImage(JSON.parseObject(o.toString()).getJSONArray("vendorItemContentDescriptions").getJSONObject(0).getString("content")));
+                } else if ("IMAGE_NO_SPACE".equals(JSON.parseObject(o.toString()).getString("contentType"))) {
+                    iList.add("http:" + JSON.parseObject(o.toString()).getJSONArray("vendorItemContentDescriptions").getJSONObject(0).getString("content"));
+                }
+            }
+//            List<String> iList = URLFecter.urlParseImage(images.getJSONArray("details").getJSONObject(0).getJSONArray("vendorItemContentDescriptions").getJSONObject(0).getString("content"));
+            imgPath.append("e:\\Downloads\\img\\").append(product.getString("productId")).append("\\")
                     .append(product.getString("itemId")).append("\\").append(product.getString("vendorItemId")).append("\\");
             FileUtils.mkdirs(imgPath.toString());
             for (String string : iList) {
@@ -76,7 +85,7 @@ public class MainClass {
 //            System.out.println(images);
             imgPath.setLength(0);
         }
-        ExcelUtils.writeExcel( "H:\\Downloads\\img\\eee.xls", "price", listTit, values);
+//        ExcelUtils.writeExcel( "e:\\Downloads\\img\\eee.xls", "price", listTit, values);
     }
 
     public static String getConfig(String[] args, String key) {
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author 97646
  * @date 2020/12/11
@@ -5,7 +9,19 @@
 public class Test {
 
     public static void main(String[] args) {
-        byte[] ad = "00068702850".getBytes();
-        ad[10] = 0x00;
+//        byte[] ad = "00068702850".getBytes();
+//        ad[10] = 0x00;
+        System.out.println(System.getProperty("user.dir"));
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\pom.xml"));
+            String str;
+            while ((str = in.readLine()) != null) {
+                System.out.println(str);
+            }
+            System.out.println(str);
+        } catch (IOException e) {
+        }
     }
+
+
 }
